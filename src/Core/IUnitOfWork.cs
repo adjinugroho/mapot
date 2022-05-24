0b67ad3bdeef1fe3516670bb0c7280b3c6b1f5e0
Adji Nugroho <adjinugroho@hotmail.com>
2022-05-24 23:15:08 +0700

    Add UnitOfWork

---
+++ src/Core/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Core.IRepositories;
+
+namespace Core
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> Complete();
+
+        ITAppLogRepository AppLogRepo { get; }
+        ITAppUserRepository AppUserRepo { get; }
+        ITAppApplicationRepository AppApplicationRepo { get; }
+        ITAppUserApplicationRepository AppUserApplicationRepo { get; }
+    }
+}
---
+++ src/Data.EF/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using Core;
+using Core.IRepositories;
+using Data.EF.Repositories;
+
+namespace Data.EF
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly ApplicationContext _context;
+
+        public ITAppLogRepository AppLogRepo { get; private set; }
+        public ITAppUserRepository AppUserRepo { get; private set; }
+        public ITAppApplicationRepository AppApplicationRepo { get; private set; }
+        public ITAppUserApplicationRepository AppUserApplicationRepo { get; private set; }
+
+        public UnitOfWork(ApplicationContext context)
+        {
+            _context = context;
+            AppLogRepo = new TAppLogRepository(_context);
+            AppUserRepo = new TAppUserRepository(_context);
+            AppApplicationRepo = new TAppApplicationRepository(_context);
+            AppUserApplicationRepo = new TAppUserApplicationRepository(_context);
+        }
+
+        public async Task<int> Complete()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
