deca9502fc5895be74e6aedaff35ccff7c4f7a94
Adji Nugroho <adjinugroho@hotmail.com>
2022-05-25 22:09:50 +0700

    Add migration

---
+++ src/Data.EF/Migrations/20220517044414_InitMigration.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.EF.Migrations
+{
+    public partial class InitMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "App_Application",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AppName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_App_Application", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "App_User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LoginAttempt = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_App_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "App_Log",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LogDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Action = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Remark = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_App_Log", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_App_Log_App_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "App_User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "App_UserApplication",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ApplicationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_App_UserApplication", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_App_UserApplication_App_Application_ApplicationId",
+                        column: x => x.ApplicationId,
+                        principalTable: "App_Application",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_App_UserApplication_App_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "App_User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "App_User",
+                columns: new[] { "Id", "CreatedAt", "CreatedBy", "IsActive", "LoginAttempt", "Name", "Password", "Role", "UpdatedAt", "UpdatedBy", "UserId" },
+                values: new object[] { new Guid("bb38fc80-3656-4b42-abcd-2e1af00d1540"), new DateTime(2022, 5, 17, 4, 44, 14, 219, DateTimeKind.Utc).AddTicks(2010), "DATA SEED", true, 0, "Superman", "$2a$11$qgJ/Q3K8xlA02q.ER6vfAO0iyvDunXSPhiK0oSUKl4sOnARwSsMs.", "Admin", null, null, "Superman" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_App_Log_UserId",
+                table: "App_Log",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_App_UserApplication_ApplicationId",
+                table: "App_UserApplication",
+                column: "ApplicationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_App_UserApplication_UserId",
+                table: "App_UserApplication",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "App_Log");
+
+            migrationBuilder.DropTable(
+                name: "App_UserApplication");
+
+            migrationBuilder.DropTable(
+                name: "App_Application");
+
+            migrationBuilder.DropTable(
+                name: "App_User");
+        }
+    }
+}
--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,15 +1,20 @@
+using Infrastructure;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services
+    .AddAppDbContext(builder.Configuration.GetConnectionString("DefaultConnection"))
+    .AddAppRepositories()
+    .AddAppServices();
+
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
@@ -18,6 +23,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
