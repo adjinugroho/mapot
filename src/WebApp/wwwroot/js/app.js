a65fada4467a3f865bfa032afc1cbad63db662e2
Adji Nugroho <adjinugroho@hotmail.com>
2022-05-25 22:05:19 +0700

    Add base web template
    * Use AdminLTE 3 for web template

---
+++ src/WebApp/wwwroot/js/app.js
@@ -0,0 +1,37 @@
+﻿function ShowNotification(type, message) {
+    var toastClass = '';
+    var toastTitle = '';
+    var toastIcon = '';
+
+    switch (type) {
+        case 'Error':
+            toastClass = 'bg-danger';
+            toastTitle = 'Error';
+            toastIcon = 'fas fa-times';
+            break;
+        case 'Success':
+            toastClass = 'bg-success';
+            toastTitle = 'Success';
+            toastIcon = 'fas fa-check';
+            break;
+        case 'Info':
+            toastClass = 'bg-info';
+            toastTitle = 'Info';
+            toastIcon = 'fas fa-envelope';
+            break;
+        case 'Warning':
+            toastClass = 'bg-warning';
+            toastTitle = 'Warning';
+            toastIcon = 'fas fa-exclamation';
+            break;
+    }
+
+    $(document).Toasts('create', {
+        class: toastClass,
+        autohide: true,
+        delay: 3000,
+        icon: toastIcon,
+        title: toastTitle,
+        body: message
+    })
+};
